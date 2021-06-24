147e99aee672486d593d33b899ee2d4cad882a91
Ianmoone <Khapankov.anton@gmail.com>
2021-06-24 12:04:02 +0300

    файлы для локаторов и главной страницы

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class MainPageLocators:
+    MAIM_PAGE = (By.CSS_SELECTOR, "#base_page")
