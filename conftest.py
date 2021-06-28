23c7767d872f5388cc890aa38f84ea9ad02eba1f
Ianmoone <Khapankov.anton@gmail.com>
2021-06-28 14:34:54 +0300

    проверки переходов по спискам

--- conftest.py
+++ conftest.py
@@ -1,34 +1,11 @@
-import time
-
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
-def pytest_addoption(parser):
-    parser.addoption('--language',
-                     action='store',
-                     default='en',
-                     help="Choose language: ru, en, ...(etc.)")
+@pytest.fixture
+def browser():
+    browser = webdriver.Chrome()
+    browser.set_window_size(1920, 1080)
 
-
-@pytest.fixture(scope="function")
-def browser(request):
-    browser_name = "chrome"
-    user_language = request.config.getoption("language")
-    if browser_name == "chrome":
-        options = Options()
-        options.add_experimental_option('prefs',
-                                        {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-    browser.maximize_window()
     yield browser
-    print("\nquit browser..")
-    time.sleep(1)
     browser.quit()
